1b326a340564e5ea13f986d9bdc91aff1970efab
edo888 <edo888@021640c9-ec57-0ddd-77d2-c16907957f9a>
2011-01-12 12:38:40 +0000

    complaints list search filter added

--- trunk/com_cls/views/complaints/tmpl/default.php
+++ trunk/com_cls/views/complaints/tmpl/default.php
@@ -18,16 +18,16 @@ $processed_complaint_warning_period = (int) $config->get('processed_complaint_wa
 jimport('joomla.filter.output');
 ?>
 <h3>Complaints List</h3>
-<form action="index.php?option=com_cls" method="post" name="adminForm">
+<form action="<?php echo JURI::base(); ?>index.php?option=com_cls" method="post" name="adminForm">
 
 <table>
     <tr>
-        <!--td align="left" width="100%">
+        <td align="left" width="100%">
             <?php echo JText::_('Filter'); ?>:
             <input type="text" name="search" id="search" value="<?php echo $this->lists['search'];?>" class="text_area" onchange="document.adminForm.submit();" />
             <button onclick="this.form.submit();"><?php echo JText::_('Go'); ?></button>
-            <button onclick="document.getElementById('search').value='';this.form.submit();"><?php echo JText::_('Reset'); ?></button>
-        </td-->
+            <!--button onclick="document.getElementById('search').value='';this.form.submit();"><?php echo JText::_('Reset'); ?></button-->
+        </td>
         <td nowrap="nowrap">
             <?php echo $this->lists['area']; ?>
             <?php echo $this->lists['source']; ?>
