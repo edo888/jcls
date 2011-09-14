1dea3a5df42f673f15eb04c6a75f8adb2c5fa1d7
edo888 <edo888@021640c9-ec57-0ddd-77d2-c16907957f9a>
2011-09-14 12:48:17 +0000

    Issue 10 front-end enhanced

--- com_cls/views/reports/tmpl/default.php
+++ com_cls/views/reports/tmpl/default.php
@@ -8,27 +8,49 @@
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
+
+JHTML::_('behavior.calendar');
 ?>
-<h3>Complaints Downloads</h3>
+<h3>Statistics Period</h3>
+<form action="<?php JURI::base(true); ?>/index.php" method="post" name="adminForm">
+<table>
+    <tr>
+        <td><?php echo JText::_('Start Date'); ?></td>
+        <td><input class="inputbox" type="text" name="startdate" id="startdate" size="25" maxlength="25" value="<?php echo $this->startdate; ?>" /> <input type="reset" class="button" value="..." onclick="return showCalendar('startdate','%Y-%m-%d');" /></td>
+    </tr>
+    <tr>
+        <td><?php echo JText::_('End Date'); ?></td>
+        <td><input class="inputbox" type="text" name="enddate" id="enddate" size="25" maxlength="25" value="<?php echo $this->enddate; ?>" /> <input type="reset" class="button" value="..." onclick="return showCalendar('enddate','%Y-%m-%d');" /></td>
+    </tr>
+</table>
+<br />
+<input type="submit" name="submit" value="Submit" />
+<input type="hidden" name="option" value="com_cls" />
+<input type="hidden" name="view" value="reports" />
+<?php echo JHTML::_( 'form.token' ); ?>
+</form>
+
+<h3>Complaint Downloads</h3>
+<a href="index.php?option=com_cls&amp;task=download_report&period=period">Download Selected Period</a><br />
 <a href="index.php?option=com_cls&amp;task=download_report&period=current_month">Download Current Month</a><br />
 <a href="index.php?option=com_cls&amp;task=download_report&period=prev_month">Download Previous Month</a><br />
 <a href="index.php?option=com_cls&amp;task=download_report&period=all">Download All</a>
 
-<h3>Summary of Complaints</h3>
+<h3>Summary of Complaint</h3>
 <i>Complaints Received Per Day:</i> <?php echo $this->complaints_received_per_day ?> <small style="color:#cc0000;"><?php echo $this->complaints_received_growth ?></small><br />
 <i>Complaints Processed Per Day:</i> <?php echo $this->complaints_processed_per_day ?> <small style="color:#cc0000;"><?php echo $this->complaints_processed_growth ?></small><br />
 <i>Complaints Resolved Per Day:</i> <?php echo $this->complaints_resolved_per_day ?> <small style="color:#cc0000;"><?php echo $this->complaints_resolved_growth ?></small><br />
 <i>Number of Complaints Received:</i> <?php echo $this->complaints_received ?><br />
 <i>Number of Complaints Resolved:</i> <?php echo $this->complaints_resolved ?><br />
 <i>Number of Complaints Outstanding:</i> <?php echo $this->complaints_outstanding ?><br />
-<i>Number of With Delayed Resolution:</i> <?php echo $this->complaints_delayed ?><br />
+<i>Number of Complaints with Delayed Resolution:</i> <?php echo $this->complaints_delayed ?><br />
 
 <br /><small><i>The averages are based on <?php echo $this->statistics_period ?> days period data.</i></small>
 
-<h3>Complaints Statistics</h3>
-<img src="<?php echo $this->complaints_per_day_link ?>" alt="complaints statistics" />
+<h3>Complaint Statistics</h3>
+<img src="<?php echo $this->complaints_per_day_link ?>" alt="complaints statistics :: drawing failed, select shorter period" />
 
-<h3>Complaints Map</h3>
+<h3>Complaint Map</h3>
 <div id="map" style="width:900px;height:500px;"></div>
 <script type="text/javascript">
 //<![CDATA[
