239cfc41157cb5cb4c3ba250607525683cad0de2
edo888 <edo888@021640c9-ec57-0ddd-77d2-c16907957f9a>
2011-09-19 11:45:11 +0000

    Fix for Issue 22

--- com_cls/cls.html.php
+++ com_cls/cls.html.php
@@ -938,7 +938,7 @@ class CLSView {
             <tr>
                 <td class="key" valign="top">
                     <label for="path">
-                        <?php echo JText::_( 'Location' ); ?>
+                        <?php echo JText::_( 'Location where Issue was Identified' ); ?>
                     </label>
                 </td>
                 <td>
