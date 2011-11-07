5a4af00dc9111c36fd49b27e19bb50ca4a0fc7c9
edo888 <edo888@021640c9-ec57-0ddd-77d2-c16907957f9a>
2011-11-07 08:01:25 +0000

    uninstall script fixed

--- trunk/com_cls/admin/uninstall.cls.sql
+++ trunk/com_cls/admin/uninstall.cls.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS `#__complaint_notifications`;
 DROP TABLE IF EXISTS `#__complaint_pictures`;
 DROP TABLE IF EXISTS `#__complaint_contracts`;
 DROP TABLE IF EXISTS `#__complaint_sections`;
-DROP TABLE IF EXISTS `#__complaint_message_queue`
+DROP TABLE IF EXISTS `#__complaint_message_queue`;
 DROP TABLE IF EXISTS `#__complaint_areas`;
 DROP TABLE IF EXISTS `#__complaint_support_groups`;
 DROP TABLE IF EXISTS `#__complaint_support_groups_users_map`;
