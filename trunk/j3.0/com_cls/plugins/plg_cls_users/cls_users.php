6ff0bdcf80631fa21e0d7e044fc27830bfd632fe
edo888 <edo888@021640c9-ec57-0ddd-77d2-c16907957f9a>
2015-02-01 15:14:14 +0000

    CLS user plugin added to control access on admin side

---
+++ trunk/j3.0/com_cls/plugins/plg_cls_users/cls_users.php
@@ -0,0 +1,46 @@
+<?php
+/**
+* @version   $Id$
+* @package   CLS
+* @copyright Copyright (C) 2008 - 2015 Edvard Ananyan. All rights reserved.
+* @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+*/
+
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.plugin.plugin');
+
+/**
+ * CLS users plugin
+ *
+ */
+class  plgSystemCLS_Users extends JPlugin {
+
+    function __construct( &$subject ) {
+        parent::__construct( $subject );
+    }
+    
+    /**
+     * Lock the admin bar and redirect the user to the complaints component
+     *
+     * @access public
+     */
+    function onAfterDispatch() {
+        $app = JFactory::getApplication();
+        
+        if(!$app->isAdmin())
+            return;
+            
+        $user = JFactory::getUser();
+        $user_type = $user->getParam('role', 'Guest');
+        
+        if($user_type != 'System Administrator' or !$user->authorise('core.admin')) {
+            if(JRequest::getVar('option') != 'com_cls') {
+                JRequest::setVar('option', 'com_cls');
+                $app->redirect('index.php?option=com_cls');
+            }
+
+            JRequest::setVar('hidemainmenu', 1);
+        }
+    }
+}
--- trunk/j3.0/com_cls/scriptfile.php
+++ trunk/j3.0/com_cls/scriptfile.php
@@ -21,11 +21,29 @@ class com_clsInstallerScript {
      * @return void
      */
     function install($parent) {
-	    // backup old user.xml file
-	    copy('components/com_users/models/forms/user.xml', 'components/com_users/models/forms/user.xml.bak');
-	    
+        // backup old user.xml file
+        if(copy('components/com_users/models/forms/user.xml', 'components/com_users/models/forms/user.xml.bak'))
+            echo '<p>'.JText::_('Joomla user parameters file backed up successfully').'</p>';
+        else
+            echo '<p>'.JText::_('Joomla user parameters file failed to be backed up').'</p>';
+        
         // update user.xml file
-        file_put_contents('components/com_users/models/forms/user.xml', file_get_contents(dirname(__FILE__).'/user_extend.xml'));
+        if(file_put_contents('components/com_users/models/forms/user.xml', file_get_contents(dirname(__FILE__).'/user_extend.xml')))
+            echo '<p>'.JText::_('Joomla user parameters extended successfully').'</p>';
+        else
+            echo '<p>'.JText::_('Joomla user parameters failed to be extended').'</p>';
+        
+        // installing plugin
+        $plugin_installer = new JInstaller;
+        if($plugin_installer->install(dirname(__FILE__).DIRECTORY_SEPARATOR.'plugins'.DIRECTORY_SEPARATOR.'plg_cls_users'))
+            echo '<p>'.JText::_('CLS Users plugin installed successfully').'</p>';
+        else
+            echo '<p>'.JText::_('CLS Users plugin installation failed').'</p>';
+        
+        // enabling plugin
+        $db = JFactory::getDBO();
+        $db->setQuery('update #__extensions set enabled = 1 where element = "cls_users" and folder = "system"');
+        $db->query();
     }
 
     /**
@@ -34,7 +52,18 @@ class com_clsInstallerScript {
      * @return void
      */
     function uninstall($parent) {
-	    // todo: revert user.xml file
+        // todo: revert user.xml file
+        
+        $db = JFactory::getDBO();
+        
+        // uninstalling plugin
+        $plugin_installer = new JInstaller;
+        $db->setQuery("select extension_id from #__extensions where name = 'System - CLS Users' and type = 'plugin' and element = 'cls_users'");
+        $cis_plugin = $db->loadObject();
+        if($plugin_installer->uninstall($cis_plugin->extension_id))
+            echo '<p>'.JText::_('CLS Users plugin uninstalled successfully').'</p>';
+        else
+            echo '<p>'.JText::_('CLS Users plugin uninstallation failed').'</p>';
     }
 
     /**
